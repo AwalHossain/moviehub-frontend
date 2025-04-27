9b4fe60bcd2b5f2ce1b727640516956f158c72b7
Awalhossain <waqin21@gmail.com>
2025-04-27 22:59:12 +0600

    fix: Adjust z-index in Navbar component for improved layering in UI

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = async () => {
     // const { registration, login } = dictionary;
 
     return (
-        <nav className="absolute top-0 left-0 right-0 z-20 px-2 md:px-6 py-4">
+        <nav className="absolute top-0 left-0 right-0  px-2 md:px-6 py-4 z-[999]">
             <div className="flex items-center justify-between space-x-6">
                 <div className="w-full">
                     {/* Pass dictionary.navLink and dictionary.profile as locale */}
