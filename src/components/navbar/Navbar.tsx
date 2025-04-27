c2246c410f3e65e66caf3c667f9da7eed7788ee2
Awalhossain <waqin21@gmail.com>
2025-04-27 16:29:29 +0600

    Add Navbar component with basic structure and NavState integration

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import NavState from "./NavState";
+
+
+const Navbar = async () => {
+
+    // const { registration, login } = dictionary;
+
+    return (
+        <nav className="absolute top-0 left-0 right-0 z-20 px-2 md:px-6 py-4">
+            <div className="flex items-center justify-between space-x-6">
+                <div className="w-full">
+                    {/* Pass dictionary.navLink and dictionary.profile as locale */}
+                    <NavState />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
