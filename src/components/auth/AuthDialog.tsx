d2b2170c661fbe621513604ea2264c6d07b704eb
Awalhossain <waqin21@gmail.com>
2025-04-28 14:19:27 +0600

    feat: Create AuthDialog component for managing authentication modal display

---
+++ src/components/auth/AuthDialog.tsx
@@ -0,0 +1,32 @@
+"use client"
+import {
+    Dialog,
+    DialogContent
+} from "@/components/ui/dialog";
+
+interface AuthDialogProps {
+    children: React.ReactNode;
+    onOpenChange?: (open: boolean) => void;
+    isOpen?: boolean;
+    setIsOpen?: (open: boolean) => void;
+}
+
+const AuthDialog: React.FC<AuthDialogProps> = ({
+    children,
+    isOpen,
+    onOpenChange
+}) => {
+
+    return (
+        <Dialog onOpenChange={onOpenChange} open={isOpen}>
+            <DialogContent className="w-[90vw] max-w-[530px] opacity-90 border-2 border-custom-border-gray-light rounded-2xl sm:rounded-3xl px-0">
+                <div className="p-4 sm:p-6 md:p-[1px_24px] gap-4 sm:gap-6">
+                    {children}
+                </div>
+
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default AuthDialog;
