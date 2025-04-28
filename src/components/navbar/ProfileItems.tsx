e4827223fbb059695b7943f70ef9fe025b7656d1
Awalhossain <waqin21@gmail.com>
2025-04-28 15:01:53 +0600

    feat: Update ProfileItems component to include Dashboard link and change labels to English

--- src/components/navbar/ProfileItems.tsx
+++ src/components/navbar/ProfileItems.tsx
@@ -1,28 +1,28 @@
-import { LogOut, UserIcon } from "lucide-react";
+import { LayoutDashboard, LogOut, UserIcon } from "lucide-react";
 
 const ProfileItems = [
     {
         key: "MyAccount",
-        label: "আমার অ্যাকাউন্ট",
+        label: "My Account",
         url: "/account",
         icon: (
-            <UserIcon className="w-5 h-5 text-custom-content-tertiary" />
+            <UserIcon className="w-5 h-5 text-white lg:text-custom-content-tertiary group-hover:text-custom-content-white" />
         ),
     },
-    // {
-    //     key: "MyInformation",
-    //     label: "আমার দেওয়া তথ্যগুলো",
-    //     url: "#",
-    //     icon: (
-    //         <ListIcon className="w-5 h-5 text-custom-content-tertiary group-hover:text-custom-content-white" />
-    //     ),
-    // },
+    {
+        key: "Dashboard",
+        label: "Dashboard",
+        url: "/dashboard",
+        icon: (
+            <LayoutDashboard className="w-5 h-5 text-white lg:text-custom-content-tertiary group-hover:text-custom-content-white" />
+        ),
+    },
     {
         key: "Logout",
-        label: "লগ আউট",
+        label: "Logout",
         url: "#",
         icon: (
-            <LogOut className="w-5 h-5 text-custom-content-tertiary" />
+            <LogOut className="w-5 h-5 text-white lg:text-custom-content-tertiary group-hover:text-custom-content-white" />
         ),
         action: "logout",
     },
