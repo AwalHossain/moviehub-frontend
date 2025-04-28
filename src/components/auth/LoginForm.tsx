43fe1c149a0070db5a9fb9fb84d82884bc8d7d70
Awalhossain <waqin21@gmail.com>
2025-04-28 14:17:08 +0600

    feat: Create LoginForm component for user authentication with error handling and loading state

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,95 @@
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { setAuthToken } from '@/action/set-cookie';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { getAxiosError } from '@/lib/getAxiosError';
+import { SignIn } from '@/services/auth';
+import { AxiosError } from 'axios';
+import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+
+interface LoginFormProps {
+    onRegistration: () => void
+    onClose: () => void
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onRegistration }) => {
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        setIsLoading(true);
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const username = formData.get('username') as string;
+        const password = formData.get('password') as string;
+        try {
+            const response = await SignIn(username, password);
+            if (response && response.access_token) {
+                await setAuthToken('access_token', response.access_token);
+                toast.success('Login successful!');
+                window.location.href = "/home";
+            } else {
+                throw new Error('Invalid response from server');
+            }
+            setIsLoading(false);
+        } catch (error: any) {
+            console.error('Error starting or resuming exam session: ', error.response);
+            if (error instanceof AxiosError) {
+                const err = getAxiosError(error);
+                toast.error(`${err.message}, ${err.status}`);
+            }
+            setIsLoading(false);
+        }
+
+    }
+
+
+    return (
+        <div className="w-full">
+            <div className="flex flex-col items-center my-4 rounded-2xl sm:rounded-3xl gap-2 sm:gap-3">
+                <div className="text-custom-content-primary text-center text-xl sm:text-heading-medium font-bold">Login</div>
+                <div className="text-custom-content-secondary text-sm sm:text-body-17 text-center">
+                    Login easily with your account
+                </div>
+            </div>
+            <div className="flex flex-col gap-4 w-full max-w-full sm:max-w-md mx-auto">
+
+
+                <form onSubmit={handleLogin} className='flex flex-col gap-2 mb-4'>
+                    <div className="flex flex-col gap-2 mb-4">
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="email" className="text-custom-content-primary text-[14px] sm:text-[16px]">Email</label>
+                            <Input type="email" name="email" placeholder="Email" className='text-custom-content-primary py-6 rounded-xl' />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="password" className="text-custom-content-primary text-[14px] sm:text-[16px]">Password</label>
+                            <Input type="password" name="password" placeholder="Password" className='text-custom-content-primary py-6 rounded-xl' />
+                        </div>
+                    </div>
+                    <Button
+                        type="submit"
+                        variant="default"
+                        className="flex cursor-pointer items-center justify-center gap-2 w-full py-3 sm:py-3 px-2 sm:px-3 rounded-lg shadow-sm transition-colors"
+                        disabled={isLoading}>
+                        {isLoading ? <Loader2 className="w-4 h-4 animate-spin" /> : "Login"}
+                    </Button>
+                </form>
+                <div className="flex flex-col items-center justify-center">
+                    <div className="w-full">
+                        <div className="w-full h-[1px] bg-custom-border-gray-light"></div>
+                        <p className="text-custom-content-secondary pt-3 text-sm sm:text-body-17 text-center">
+                            Don&apos;t have an account? <br />
+                            <button onClick={onRegistration} className="text-custom-green font-bold cursor-pointer">Create Account</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default LoginForm
