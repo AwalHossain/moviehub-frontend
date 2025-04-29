70cb3d5b4e6d456bf3df47decfb367c7af223f7e
Awalhossain <waqin21@gmail.com>
2025-04-29 15:55:03 +0600

    feat: Refactor authentication service to enhance sign-up and sign-in processes with improved data structure and error handling

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,29 +1,69 @@
-
 import { clientFetch } from "../client-fetch";
 import { ENDPOINTS } from "../endpoints";
 
-interface SignUpResponse {
-  access_token: string;
+interface AuthData {
+  accessToken: string;
+  refreshToken: string;
+  user: {
+    _id: string;
+    name: string;
+    role: string;
+    isEmailVerified: boolean;
+    avatar: string;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+  };
 }
 
-export const SignUp = async (username: string, password: string) => {
+
+export const SignUp = async (name: string, email: string, password: string): Promise<AuthData | undefined> => {
   const url = `${ENDPOINTS["SIGNUP"]}`;
-  const response = await clientFetch.post<SignUpResponse>(url, {
-    username,
-    password,
-  });
-  if (response.data) {
-    return response.data;
+  try {
+    const response = await clientFetch.post(url, {
+      name,
+      email,
+      password,
+    });
+    console.log(response.data, "response from signup service"); 
+    return response.data.data as AuthData;
+  } catch (error) {
+      console.error('Error during sign up:', error);
+      throw error; 
   }
 };
 
-export const SignIn = async (username: string, password: string) => {
+export const SignIn = async (email: string, password: string): Promise<AuthData> => {
   const url = `${ENDPOINTS["SIGNIN"]}`;
-  const response = await clientFetch.post<SignUpResponse>(url, {
-    username,
-    password,
-  });
-  if (response.data) {
-    return response.data;
+  try {
+    const response = await clientFetch.post(url, {
+      email,
+      password,
+    });
+    if (response.data && response.data.data) { 
+      console.log(response.data.data, "response data from signin service");
+      return response.data.data as AuthData;
+    } else {
+      console.error("SignIn successful but no data found in response");
+      throw new Error("Login failed: Invalid data structure from server");
+    }
+  } catch (error) {
+    console.error("Error during sign in:", error);
+    throw error;
   }
 };
+
+
+export const loadUser = async (): Promise<AuthData> => {
+  const url = `${ENDPOINTS["LOAD_USER"]}`;
+  try {
+    const response = await clientFetch.get(url);
+    console.log(response.data.data, "response data from load user service");
+    return response.data.data as AuthData;
+  } catch (error) {
+    console.error("Error during load user:", error);
+    throw error;
+  }
+}
+
+
