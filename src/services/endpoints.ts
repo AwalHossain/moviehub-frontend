d55a3c96e8af1c70a54a584c0cebf8f9f288d0e9
Awalhossain <waqin21@gmail.com>
2025-04-29 15:56:06 +0600

    feat: Update authentication endpoints to reflect new API structure, renaming SIGNUP and SIGNIN routes and adding LOAD_USER and TOKEN_REFRESH endpoints

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,7 +1,9 @@
 
 export const ENDPOINTS = {
   LOGIN: "/auth/login",
-  SIGNUP: "/auth/signup",
-  SIGNIN: "/auth/signin",
+  SIGNUP: "/auth/register",
+  SIGNIN: "/auth/login",
   LOGOUT: "/auth/logout",
+  LOAD_USER: "/auth/check-session",
+  TOKEN_REFRESH: "/auth/refresh-token",
 };
